5a5eb0ee1c4b55744f95d01cf9cefa3661cc0907
maawoo <marco.wolsza@gmail.com>
2020-04-19 20:11:43 +0200

    Minor changes

--- grass.py
+++ grass.py
@@ -1,35 +1,44 @@
 import os
 import sys
+from osgeo import osr
 from grass_session import Session, get_grass_gisbase
-import grass.script as gscript
-import grass.script.setup as gsetup
 
-def setup_grass(version=None, dir=None, name=None, crs=None):
+try:
+    import grass.script as gscript
+    import grass.script.setup as gsetup
+except ImportError:
+    raise ImportError("If you want to analyse your data, you need to install "
+                      "GRASS GIS.")
+
+def setup_grass(version=None, path=None, name=None, crs=None):
     """
     bla
     """
     if version is None:
         version = 'grass78'
-    if dir is None:
-        dir = './grass/'
+    if path is None:
+        path = './grass/'
     if name is None:
         name = 'my_awesome_grass_db'
+
     if crs is None:
         crs = '4326'
+    else:
+        if osr.SpatialReference().ImportFromEPSG(int(crs)) == 6:
+            raise ValueError("The provided EPSG code is not valid.")
 
     # General GRASS setup
-    grassbin = version
-    os.environ['GRASSBIN'] = grassbin
+    os.environ['GRASSBIN'] = version
     gisbase = get_grass_gisbase()
     os.environ['GISBASE'] = gisbase
     sys.path.append(os.path.join(os.environ['GISBASE'], 'bin'))
     sys.path.append(os.path.join(os.environ['GISBASE'], 'lib'))
     sys.path.append(os.path.join(os.environ['GISBASE'], 'scripts'))
     sys.path.append(os.path.join(os.environ['GISBASE'], 'etc', 'python'))
-    #os.environ['PROJ_LIB'] = 'C:\\OSGeo4W64\\share\\proj'
+    # os.environ['PROJ_LIB'] = 'C:\\OSGeo4W64\\share\\proj'
 
     # User-defined settings
-    gisdb = dir
+    gisdb = path
     location = name
     mapset = 'PERMANENT'
 
@@ -47,18 +56,24 @@ def setup_grass(version=None, dir=None, name=None, crs=None):
 
 setup_grass(name='Spain_Donana', crs='32629')
 
-##################################################################################
+###############################################################################
 
 data_path = "./data/test_data"
-test_file = "./data/test_data" \
-            "/S1A__IW___A_20150107T182611_147_VV_grd_mli_norm_geo_db.tif "
+file1 = "./data/test_data" \
+            "/S1A__IW___A_20150107T182611_147_VV_grd_mli_norm_geo_db.tif"
+file2 = "./data/test_data" \
+             "/S1A__IW___A_20150303T181753_74_VV_grd_mli_norm_geo_db.tif"
 
 # Check if any raster files have already been imported
-for rast in gscript.list_strings(type = 'rast'):
+for rast in gscript.list_strings(type='rast'):
     print(rast)
 
-map = "test_ras"
+# Import files
+gscript.run_command("r.in.gdal", input=file1, output="test1")
+gscript.run_command("r.in.gdal", input=file2, output="test2")
 
-gscript.run_command("r.in.gdal", input=test_file, output=map)
-gscript.run_command("r.info", map=map)
+# Print basic info
+gscript.run_command("r.info", map="test2")
 
+# Test simple raster calculation
+gscript.mapcalc("test_out = $a + $b", a="test1", b="test2")

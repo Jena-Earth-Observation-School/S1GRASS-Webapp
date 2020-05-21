be5a348a3e716111303c616e3fce4c3f12c40080
maawoo <marco.wolsza@gmail.com>
2020-05-21 17:36:35 +0200

    Adding files

---
+++ flask_webapp/__init__.py
@@ -0,0 +1,7 @@
+from flask import Flask
+#from flask_sqlalchemy import SQLAlchemy
+#import sqlite3
+
+app = Flask(__name__)
+
+from flask_webapp import routes
---
+++ main_script.py
@@ -0,0 +1,2 @@
+from flask_webapp import app
+

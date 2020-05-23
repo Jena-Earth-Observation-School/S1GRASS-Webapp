35e6a35b00749a1c695a384e3caa6389c7a674c8
maawoo <marco.wolsza@gmail.com>
2020-05-23 18:40:02 +0200

    Add flask shell context

--- main_script.py
+++ main_script.py
@@ -1,2 +1,9 @@
-from flask_webapp import app
+from flask_webapp import app, db
+from flask_webapp.models import Scene, Metadata, Geometry
 
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db,
+            'Scene': Scene,
+            'Metadata': Metadata,
+            'Geometry': Geometry}

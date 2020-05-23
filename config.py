065cca54476909e3ba3b9e6e96941de816749096
maawoo <marco.wolsza@gmail.com>
2020-05-23 19:58:19 +0200

    Change how path to data is handled

--- config.py
+++ config.py
@@ -1,5 +1,19 @@
 import os
 
+###### CHANGE DATA DIRECTORY HERE ######
+
+data_dir = "D:/GEO450_data"
+
+########################################
+
+data_dir = os.path.abspath(data_dir)
+
+if not os.path.exists(data_dir):
+    raise ImportError(data_dir, " does not exist. Please change the "
+                                "variable 'data_dir' in 'config.py' to a"
+                                "path containing the datasets you want to "
+                                "work with.")
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 sqlite_dir = os.path.join(basedir, 'sqlite')
 grass_dir = os.path.join(basedir, 'grass')
@@ -10,7 +24,12 @@ if not os.path.exists(sqlite_dir):
 if not os.path.exists(grass_dir):
     os.makedirs(grass_dir)
 
+
 class Config(object):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' \
                               + os.path.join(sqlite_dir, 's1_webapp.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class DataPath(object):
+    path = data_dir

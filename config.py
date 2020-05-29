a7cb89771ec8fc4bb667e3c1a0ee41067555950b
maawoo <marco.wolsza@gmail.com>
2020-05-29 15:29:39 +0200

    Added comments and new paths

--- config.py
+++ config.py
@@ -6,6 +6,8 @@ data_dir = "D:/GEO450_data"
 
 ########################################
 
+## Convert to normalized path (just in case) and print error message if it
+## doesn't exist
 data_dir = os.path.abspath(data_dir)
 
 if not os.path.exists(data_dir):
@@ -14,22 +16,32 @@ if not os.path.exists(data_dir):
                                 "path containing the datasets you want to "
                                 "work with.")
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-sqlite_dir = os.path.join(basedir, 'sqlite')
-grass_dir = os.path.join(basedir, 'grass')
+## Save sqlite and grass stuff in subdirectories of the data directory,
+## so the user can access and work with both outside of this webapp.
+sqlite_dir = os.path.join(data_dir, 'sqlite')
+grass_dir = os.path.join(data_dir, 'grass')
+grass_dir_out = os.path.join(grass_dir, 'output')
 
 if not os.path.exists(sqlite_dir):
     os.makedirs(sqlite_dir)
-
 if not os.path.exists(grass_dir):
     os.makedirs(grass_dir)
+if not os.path.exists(grass_dir_out):
+    os.makedirs(grass_dir_out)
 
-
+## SQLite configurations that are also imported in __init__.py
 class Config(object):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' \
                               + os.path.join(sqlite_dir, 's1_webapp.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-
-class DataPath(object):
+## Save paths as classes so they can easily imported elsewhere (e.g. as
+## 'Data.path' or 'Grass.path')
+class Data(object):
     path = data_dir
+
+class Grass(object):
+    path = grass_dir
+
+class Database(object):
+    path = sqlite_dir

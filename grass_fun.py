04bf4d3ae00a10edb55f928ca173ad314e35802e
maawoo <marco.wolsza@gmail.com>
2020-06-05 20:56:39 +0200

    The GRASS output directory will now be checked first before calculating the footprint for a scene.

--- grass_fun.py
+++ grass_fun.py
@@ -56,59 +56,61 @@ def setup_grass(version=None, path=None, name=None, crs=None):
     print(gscript.gisenv())
 
 
-def get_footprint(raster, out_path=None):
-    """
-    bla
+def get_footprint(raster):
+    """Uses GRASS to calculate the exact footprint (not extent!) of a raster.
     :param raster: full path e.g.
     'D:\\GEO450_data\\S1A__IW___A_20150320T182611_147_VV_grd_mli_norm_geo_db.tif'
-    :return: Footprint as a GeoJSON file located in out_path\footprints
+    :return: GeoJSON file located in out_path\footprints.
     """
+    ## Create output folder if it doesn't exist already.
+    out_path = os.path.join(Grass.path, 'output\\footprints')
+    if not os.path.exists(out_path):
+        os.makedirs(out_path)
 
-    if out_path is None:
-        out_path = os.path.join(Grass.path, 'output\\footprints')
-        if not os.path.exists(out_path):
-            os.makedirs(out_path)
-
+    ## Define filenames and -path.
     ras_name = os.path.basename(raster)
     ras_name = ras_name[:-len(Path(ras_name).suffix)]
     ras_name_suffix = ras_name + '.geojson'
     ras_name_foot = ras_name + "_footprint"
     out_name = os.path.join(out_path, ras_name_suffix)
 
-    try:
-        # Set computational region
-        gscript.run_command('g.region', raster=ras_name)
-    except:
-        # Import file, then set computational region and continue
-        import_raster(raster, ras_name)
-        gscript.run_command('g.region', raster=ras_name)
+    ## Check if for the given scene a geojson-file already exists. If so,
+    ## then skip calculation. Else calculate footprint and save geojson-file.
+    file_list = os.listdir(out_path)
+    if any(ras_name in file for file in file_list):
+        print("Footprint for", ras_name, "already exists.")
+        return out_name
 
-    # Create temporary raster file with all values set to 1
-    gscript.mapcalc("ras_tmp = (abs($a) > 0) * 1", a=ras_name, overwrite=True)
+    else:
+        try:
+            ## Set computational region
+            gscript.run_command('g.region', raster=ras_name)
+        except:
+            ## Import file, then set computational region and continue
+            gscript.run_command("r.in.gdal", input=raster, output=ras_name,
+                                flags="e")
+            gscript.run_command('g.region', raster=ras_name)
 
-    # Create temporary vector file from "ras_tmp"
-    gscript.run_command("r.to.vect", input="ras_tmp", output="vec_tmp",
-                      type="area")
+        ## Create temporary raster file with all values set to 1
+        gscript.mapcalc("ras_tmp = (abs($a) > 0) * 1", a=ras_name, overwrite=True)
 
-    # Create convex hull from vec_tmp
-    gscript.run_command("v.hull", input="vec_tmp",
-                      output=ras_name_foot,
-                      overwrite=True)
+        ## Create temporary vector file from "ras_tmp"
+        gscript.run_command("r.to.vect", input="ras_tmp", output="vec_tmp",
+                          type="area")
 
-    # Export as GeoJSON
-    gscript.run_command("v.out.ogr", input=ras_name_foot,
-                      output=out_name, format="GeoJSON", overwrite=True)
+        ## Create convex hull from vec_tmp
+        gscript.run_command("v.hull", input="vec_tmp",
+                          output=ras_name_foot,
+                          overwrite=True)
 
-    # Remove tmp-files from mapset
-    gscript.run_command("g.remove", type="raster", name="ras_tmp", flags="f")
-    gscript.run_command("g.remove", type="vector", name="vec_tmp", flags="f")
-    gscript.run_command("g.remove", type="vector", name=ras_name_foot,
-                      flags="f")
+        ## Export as GeoJSON
+        gscript.run_command("v.out.ogr", input=ras_name_foot,
+                          output=out_name, format="GeoJSON", overwrite=True)
 
-    return out_name
+        ## Remove tmp-files from mapset
+        gscript.run_command("g.remove", type="raster", name="ras_tmp", flags="f")
+        gscript.run_command("g.remove", type="vector", name="vec_tmp", flags="f")
+        gscript.run_command("g.remove", type="vector", name=ras_name_foot,
+                          flags="f")
 
-
-def import_raster(path, name):
-
-    gscript.run_command("r.in.gdal", input=path, output=name, flags="e")
-
+        return out_name

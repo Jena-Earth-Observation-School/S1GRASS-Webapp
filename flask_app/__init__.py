3a4c40e21b82a8bbcce7ddef4dccb1691f7fc02a
maawoo <marco.wolsza@gmail.com>
2020-06-06 22:45:31 +0200

    Make everything more fancy with bootstrap

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -2,18 +2,12 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+bootstrap = Bootstrap(app)
 
 from flask_app import routes, models
-
-"""
-Flask shell commands:
-flask db init (initialize database)
-flask db migrate (create migration script after modifying db)
-flask db upgrade (execute migration script)
-flask db downgrade (undo last migration)
-"""

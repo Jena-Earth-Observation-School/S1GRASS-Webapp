05feb0c1bec96dea57b37ec8252524ade6df6374
maawoo <marco.wolsza@gmail.com>
2020-06-07 18:11:40 +0200

    Some minor changes

--- flask_app/routes.py
+++ flask_app/routes.py
@@ -1,8 +1,8 @@
 from flask import render_template
 import folium
 
-from flask_app.tables import *
 from flask_app import app
+from flask_app.tables import *
 from flask_app.models import Scene
 
 
@@ -12,29 +12,37 @@ def index():
     return render_template('home.html')
 
 
-@app.route('/map')
-def map():
-    start_coords = (46.9540700, 142.7360300)
-    folium_map = folium.Map(location=start_coords, zoom_start=14)
-    return folium_map._repr_html_()
-
-
 @app.route('/overview')
 def overview():
 
     ## Create table (html)
     table = create_overview_table()
 
-    return render_template('table.html', table=table)
+    ## Title for the html page
+    title = "Scenes currently stored in the database:"
+
+    return render_template('table.html', table=table, title=title)
 
 
 @app.route('/meta/<scene_id>')
 def meta(scene_id):
 
-    ## Get scene from database
-    s = Scene.query.filter_by(id=scene_id).first_or_404()
+    ## Get scene by id
+    s = Scene.query.filter_by(id=scene_id).\
+        first_or_404("A scene with this ID is currently "
+                     "not stored in the database.")
 
     ## Create table (html)
     table = create_meta_table(s)
 
-    return render_template('table.html', table=table)
+    ## Title for the html page
+    title = "Metadata for scene #" + scene_id
+
+    return render_template('table.html', table=table, title=title)
+
+
+@app.route('/map')
+def map():
+    start_coords = (46.9540700, 142.7360300)
+    folium_map = folium.Map(location=start_coords, zoom_start=14)
+    return folium_map._repr_html_()

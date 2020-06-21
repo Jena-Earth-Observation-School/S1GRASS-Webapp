cd8420adbf3101a6a65025c051963c8ac074acd8
maawoo <marco.wolsza@gmail.com>
2020-06-21 16:48:58 +0200

    Some folium & leaflet tests

--- flask_app/maps.py
+++ flask_app/maps.py
@@ -1,8 +1,10 @@
+from config import Grass
+from flask_app.models import Scene
+
 import folium
 from pyproj import Transformer
-import json
-
-from flask_app.models import Scene
+from osgeo import gdal
+import os
 
 
 def get_coords_from_first():
@@ -29,21 +31,38 @@ def get_coords_from_first():
 
 
 ######################
-coords = get_coords_from_first()
-map_ = folium.Map(location=coords, zoom_start=10)
 
 ## Test adding footprint to map
-scene = Scene.query.first()
-foot = scene.geo[0].footprint
-foot_geojson = json.loads(foot)
+#scene = Scene.query.first()
+#foot = scene.geo[0].footprint
+#foot_geojson = json.loads(foot)
 
-folium.GeoJson(foot_geojson, name='test_footprint').add_to(map_)
+#folium.GeoJson(foot_geojson, name='test_footprint').add_to(map_)
 
 
 ## Test adding raster to map
+coords = get_coords_from_first()
+map_ = folium.Map(location=coords, zoom_start=10)
 
+cog = os.path.join(Grass.path_out, "cog_4326\\test_cog.tif")
+tif = os.path.join(Grass.path_out, "cog_4326\\test_geotiff.tif")
 
-map_.save('map.html')
+ds = gdal.Open(cog, gdal.GA_ReadOnly)
+rb = ds.GetRasterBand(1)
+img_array = rb.ReadAsArray()
 
+lat_min = 36.732445
+lat_max = 37.266466
+lon_min = -6.594324
+lon_max = -6.147788
+bounds = [[lat_min, lon_min], [lat_max, lon_max]]
+
+[[36.732445, -6.594324], [37.266466, -6.147788]]
+
+map_.add_child(folium.raster_layers.ImageOverlay(image=img_array,
+                                                 bounds=bounds,
+                                                 opacity=0.75))
+
+map_.save('map_cog.html')
 
 
--- flask_app/static/js/leaflet_embed.js
+++ flask_app/static/js/leaflet_embed.js
@@ -1,157 +1,64 @@
-// center of the map
-var center = [50.926453, 11.587832];
 
-// The first parameter are the coordinates of the center of the map
-// The second parameter is the zoom level
-var map = L.map('map').setView(center, 11);
+var map = L.map('map').setView([37, -6.4], 10);
 
-// {s}, {z}, {x} and {y} are placeholders for map tiles
-// {x} and {y} are the map coordinates
-// {z} is the zoom level
-// {s} is the subdomain
-var layer = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> Contributors',
-    maxZoom: 18
-});
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
 
-//CartoDB map alternative
-//var layer = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
-//    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
-//});
+////////////////////////////////////////////////
+//////// OLD simple example --> empty overlay
+var imageUrl = 'D:/GEO450_data/grass/output/cog_4326/test_cog.tif',
+imageBounds = [[36.732445, -6.594324], [37.266466, -6.147788]];
 
-// Now add the layer onto the map
-map.addLayer(layer);
+L.imageOverlay(imageUrl, imageBounds).addTo(map);
 
-// add marker with popup
-//L.marker(center).addTo(map)
-//    .bindPopup("<strong>Crazy People</strong>").openPopup();
 
-// Initialise the FeatureGroup to store editable layers
-var editableLayers = new L.FeatureGroup();
-map.addLayer(editableLayers);
+//////// FROM load-file example
+var file = "file:///D:/GEO450_data/grass/output/cog_4326/test_cog.tif";
 
-///////////////////////////////////////////////////////////////////////
-//options for Leaflet.Draw to get a toolbar for drawing geometries
-var drawPluginOptions = {
-//    position: 'topright',
-    draw: {
-//        disable toolbar item by setting it to false
-        polyline: false,
-//        circle: false,
-        circlemarker: false,
-//        rectangle: false,
-        marker: false
-//    polygon: {
-//        allowIntersection: false, // Restricts shapes to simple polygons
-//        drawError: {
-//            color: '#e1e100', // Color the shape will turn when intersects
-//            message: '<strong>Oh snap!<strong> you can\'t draw that!' // Message that will show when intersect
-//        },
-//        shapeOptions: {
-//            color: '#97009c'
-//        }
-//    },
-    },
-    edit: {
-        featureGroup: editableLayers, //REQUIRED!!
-        remove: true
-    }
-};
-///////////////////////////////////////////////////////////////////////
-// Initialise the draw control and pass it the FeatureGroup of editable layers
-// L is the Leaflet library
-var drawControl = new L.Control.Draw(drawPluginOptions);
-map.addControl(drawControl);
 
-map.on('draw:created', function(e) {
-    var type = e.layerType,
-        layer = e.layer;
-//    if (type === 'marker') {
-//        layer.bindPopup('A popup!');
-//    }
-//    if (type === 'rectangle') {
-//        layer.on('mouseover', function() {
-//            alert(layer.getLatLngs());
-//        });
-//    }
-    editableLayers.addLayer(layer);
-});
+var reader = new FileReader();
 
-//execute function exportGeoJSON on clicking the export button (defined in home.html)
-//the document object represents the HTML document that is displayed in that window
-document.getElementById('export').onclick = exportGeoJSON;
+var file_data_url = reader.readAsDataURL(file)
 
-//alternative click action execution using jQuery
-//here: open a file import dialog on click of the Import button
-$('#import').on('click', importDialog);
+reader.readAsArrayBuffer(file_data_url);
+reader.onloadend = function() {
+    var arrayBuffer = reader.result;
+    parseGeoraster(arrayBuffer).then(georaster => {
+    console.log("georaster:", georaster);
 
-//import and display file-select in the map once it changes
-$("#file-select").change(importSHP);
-
-function exportGeoJSON() {
-    if (editableLayers.getLayers().length > 0){
-        // Extract GeoJSON from featureGroup
-        var data = editableLayers.toGeoJSON();
-
-        // Stringify the GeoJSON
-        var convertedData = 'data:text/json;charset=utf-8,' + encodeURIComponent(JSON.stringify(data));
-
-        var element = document.createElement('a');
-        element.setAttribute('href', convertedData);
-        element.setAttribute('download', 'drawnItems.geojson');
-
-        element.style.display = 'none';
-        document.body.appendChild(element);
-
-        element.click();
-
-        document.body.removeChild(element);
-    } else {
-        alert("no geometries found");
-    }
-}
-
-//taken from http://jsfiddle.net/ashalota/ov0p4ajh/10/
-function importSHP(){
-	var files = document.getElementById('file-select').files;
-	if (files.length == 0) {
-	  return; //do nothing if no file given yet
-  }
-
-  var file = files[0];
-
-    if (file.name.slice(-3) != 'zip'){ //Demo only tested for .zip. All others, return.
-        alert("Please provide a zip file!");
-        return;
-    } else {
-        document.getElementById('warning').innerHTML = ''; //clear warning message.
-        handleZipFile(file);
-    }
-};
-
-function importDialog() {
-    $('#file-select').trigger('click');
-}
-///////////////////////////////////////////////////////////////////////
-//More info: https://developer.mozilla.org/en-US/docs/Web/API/FileReader
-//taken from http://jsfiddle.net/ashalota/ov0p4ajh/10/
-function handleZipFile(file){
-    var reader = new FileReader();
-    reader.onload = function(){
-        if (reader.readyState != 2 || reader.error){
-            alert("cannot read file");
-            return;
-        } else {
-            convertToLayer(reader.result);
-        }
-    }
-    reader.readAsArrayBuffer(file);
-}
-
-//taken from http://jsfiddle.net/ashalota/ov0p4ajh/10/
-function convertToLayer(buffer){
-    shp(buffer).then(function(geojson){	//More info: https://github.com/calvinmetcalf/shapefile-js
-        var layer = L.geoJson(geojson).addTo(map);
-        editableLayers.addLayer(layer);
+    var layer = new GeoRasterLayer({
+        georaster: georaster,
+        opacity: 0.8,
+        resolution: 256
     });
-}
+
+    layer.addTo(map);
+
+    map.fitBounds(layer.getBounds());
+
+  });
+};
+
+//////// FROM main.js (local hosting)
+var file = "file:///D:/GEO450_data/grass/output/cog_4326/test_cog.tif";
+
+fetch(file)
+  .then(response => response.arrayBuffer())
+  .then(arrayBuffer => {
+    parse_georaster(arrayBuffer).then(georaster => {
+      console.log("georaster:", georaster);
+
+
+      var layer = new GeoRasterLayer({
+          georaster: georaster,
+          opacity: 0.8,
+          resolution: 256
+      });
+
+      layer.addTo(map);
+
+      map.fitBounds(layer.getBounds());
+
+  });
+});

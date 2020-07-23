1c285d4ac90e4bda3ffd1f6ba3564cf79126300b
maawoo <marco.wolsza@gmail.com>
2020-07-23 21:48:07 +0200

    Extend content of metadata table

--- flask_app/tables.py
+++ flask_app/tables.py
@@ -39,8 +39,9 @@ def create_overview_table():
 
 def create_meta_table(scene):
 
-    ## Query all metadata for the given scene
+    ## Query metadata for the given scene
     meta = scene.meta.all()
+    geo = scene.geo.all()
 
     ## Generate items to be listed
     items = [dict(attr='Scene ID', val=scene.id),
@@ -51,7 +52,14 @@ def create_meta_table(scene):
              dict(attr='Polarisation', val=meta[0].polarisation),
              dict(attr='Resolution (m)', val=meta[0].resolution),
              dict(attr='Band Min', val=meta[0].band_min),
-             dict(attr='Band Max', val=meta[0].band_max)]
+             dict(attr='Band Max', val=meta[0].band_max),
+             dict(attr='Columns, Rows', val=f"{geo[0].columns}, "
+                                            f"{geo[0].rows}"),
+             dict(attr='EPSG', val=geo[0].epsg),
+             dict(attr='Bounds North', val=geo[0].bounds_north),
+             dict(attr='Bounds South', val=geo[0].bounds_south),
+             dict(attr='Bounds East', val=geo[0].bounds_east),
+             dict(attr='Bounds West', val=geo[0].bounds_west)]
 
     ## Populate the table
     table = MetaTable(items)
